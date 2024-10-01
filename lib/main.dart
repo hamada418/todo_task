ab6f57fbecb2c01e837fb6da0a8c08237a96a4fe
hamada418 <42110009.Mohamed@acu.edu.eg>
2024-10-01 21:26:10 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todoiti/home_page.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
